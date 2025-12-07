41a8bcc7fb4da45e62b07e5f598d9d7380546bdb
camillacostato <10862227@polimi.it>
2025-12-07 12:36:37 +0100

    aggiunta .venv camilla

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Volumes/Install macOS Sequoia/ALUA-PYTHON/.venv"
+set -gx VIRTUAL_ENV "/Users/camilla/Desktop/III anno I semestre /LAB sintesi/ALUA-PYTHON/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
